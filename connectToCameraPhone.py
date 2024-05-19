42589a519eb951423b2e8e595c08d4f019cdac70
unclegenson <unclegenson@gmail.com>
2024-05-19 11:22:14 +0330

    play sound async checked

--- connectToCameraPhone.py
+++ connectToCameraPhone.py
@@ -1,16 +1,12 @@
 import imutils
 import numpy as np
 import cv2
-import requests
-import playsound 
-url = "http://192.168.1.103:8080/shot.jpg"
+from kivy.core.audio import SoundLoader
 
 X_axis ,Y_axis= [],[]
 
-
-async def playTheSound(sound):
-    await playsound(sound=sound)
-
+def kivy_play_sound(sound):
+    SoundLoader.load(sound).play()
 
 def reject_outliers(data, m = 3.):
     d = np.abs(data - np.median(data))
@@ -20,12 +16,11 @@ def reject_outliers(data, m = 3.):
 
 Y_distances = []
 X_distances = []
+cap = cv2.VideoCapture(0)
 
 while(True):
+    _,captured_frame = cap.read()
 
-    img_resp = requests.get(url) 
-    img_arr = np.array(bytearray(img_resp.content), dtype=np.uint8) 
-    captured_frame = cv2.imdecode(img_arr, -1) 
     captured_frame = imutils.resize(captured_frame, width=1000, height=1800)
     
     output_frame = captured_frame.copy()
@@ -35,24 +30,24 @@ while(True):
     captured_frame_bgr = cv2.medianBlur(captured_frame_bgr, 3)
     captured_frame_lab = cv2.cvtColor(captured_frame_bgr, cv2.COLOR_BGR2Lab)
     captured_frame_lab_red = cv2.inRange(captured_frame_lab, np.array([30, 150, 150]), np.array([190, 255, 255])) 
+
     sensitivity = 15
     lower_white = np.array([0,0,255-sensitivity])
     upper_white = np.array([255,sensitivity,255])  
+
     captured_frame_lab_white = cv2.inRange(captured_frame_lab, lower_white, upper_white) 
-    # Second blur to reduce more noise, easier circle detection
     captured_frame_lab_white = cv2.GaussianBlur(captured_frame_lab_red, (5, 5), 2, 2)
-    # Use the Hough transform to detect circles in the image
     circles = cv2.HoughCircles(captured_frame_lab_white, cv2.HOUGH_GRADIENT, 1, captured_frame_lab_white.shape[0] / 8, param1=100,
                                 param2=18, minRadius=2, maxRadius=20)
     
     
     if circles is None:
-        playTheSound('nothing_detected.mp3')
+        kivy_play_sound('nothing_detected.mp3')
         cv2.putText(output_frame,'0 laser detected',(50,50),cv2.FONT_HERSHEY_COMPLEX_SMALL,0.6,255)
 
     elif circles is not None:
         if len(circles[0]) > 1:
-            playTheSound('more_than_one.mp3')
+            kivy_play_sound('more_than_one.mp3')
             cv2.putText(output_frame,'more than 1 laser detected',(50,50),cv2.FONT_HERSHEY_COMPLEX_SMALL,0.6,255)
 
         else:
@@ -67,7 +62,7 @@ while(True):
 
             circles = np.round(circles[0, :]).astype("int")
             cv2.circle(output_frame, center=(circles[0, 0], circles[0, 1]), radius=circles[0, 2], color=(0, 255, 0), thickness=2)
-            cv2.putText(output_frame,'laser detected',(50,50),cv2.FONT_HERSHEY_COMPLEX_SMALL,0.6,255)
+            cv2.putText(output_frame,'1 laser detected',(50,50),cv2.FONT_HERSHEY_COMPLEX_SMALL,0.6,255)
 
             X_axis = reject_outliers(np.array(X_axis))
             Y_axis = reject_outliers(np.array(Y_axis))
@@ -84,7 +79,7 @@ while(True):
                 for x_distance in X_distances:
                     if abs(x_distance) >=  2 * abs(sum(X_distances) / len(X_distances)):
                         cv2.putText(output_frame,'Paresh X',(50,50),cv2.FONT_HERSHEY_COMPLEX_SMALL,0.6,255)                    
-                        playTheSound('something_detected.mp3')
+                        kivy_play_sound('something_detected.mp3')
                         X_axis = list(X_axis)
                         X_axis.clear()
                         Y_axis = list(Y_axis)
@@ -95,7 +90,7 @@ while(True):
 
                     elif abs(x_distance) <=  abs(sum(X_distances) / len(X_distances)) / 3:
                         cv2.putText(output_frame,'Mane X',(50,50),cv2.FONT_HERSHEY_COMPLEX_SMALL,0.6,255)
-                        playTheSound('something_detected.mp3')
+                        kivy_play_sound('something_detected.mp3')
                         X_axis = list(X_axis)
                         X_axis.clear()
                         Y_axis = list(Y_axis)
@@ -118,7 +113,7 @@ while(True):
                     for y_distance in Y_distances:
                         if abs(y_distance) >=  2 * abs(sum(Y_distances) / len(Y_distances)):
                             cv2.putText(output_frame,'Paresh Y',(50,50),cv2.FONT_HERSHEY_COMPLEX_SMALL,0.6,255)
-                            playTheSound('something_detected.mp3')
+                            kivy_play_sound('something_detected.mp3')
                             X_axis = list(X_axis)
                             X_axis.clear()
                             Y_axis = list(Y_axis)
@@ -129,7 +124,7 @@ while(True):
 
                         elif abs(y_distance) <=  abs(sum(Y_distances) / len(Y_distances)) / 3:
                             cv2.putText(output_frame,'Mane Y',(50,50),cv2.FONT_HERSHEY_COMPLEX_SMALL,0.6,255)
-                            playTheSound('something_detected.mp3')
+                            kivy_play_sound('something_detected.mp3')
                             X_axis = list(X_axis)
                             X_axis.clear()
                             Y_axis = list(Y_axis)
@@ -138,7 +133,9 @@ while(True):
                             X_distances.clear()
 
                             break
-                    
+            else:
+                X_axis = list(X_axis)
+                Y_axis = list(Y_axis)        
 
     cv2.imshow('frame', output_frame)
     if cv2.waitKey(1) & 0xFF == ord('q'):
